3c48b0af3c83e9058823cea2f5342d108f9e435b
Tim <tim.guenther@live.de>
2015-07-31 17:06:19 +0200

    add new classes for sso protocols

---
+++ src/main/java/de/rub/nds/burp/utilities/protocols/OpenIDConnect.java
@@ -0,0 +1,49 @@
+/**
+ * EsPReSSO - Extension for Processing and Recognition of Single Sign-On Protocols.
+ * Copyright (C) 2015/ Tim Guenther and Christian Mainka
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package de.rub.nds.burp.utilities.protocols;
+
+import burp.IBurpExtenderCallbacks;
+import burp.IParameter;
+
+/**
+ *
+ * @author Tim Guenther
+ */
+public class OpenIDConnect extends SSOProtocol{
+
+    public OpenIDConnect() {
+    }
+
+    public OpenIDConnect(IParameter param, IBurpExtenderCallbacks callbacks) {
+        super(param, callbacks);
+        super.setProtocol(OPENID_CONNECT);
+    }
+    
+
+    @Override
+    public String decode(String input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String findID() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
