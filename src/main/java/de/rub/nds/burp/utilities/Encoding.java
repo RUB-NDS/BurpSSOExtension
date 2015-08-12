22e36318f6c41000d3bc097d7c7d872f8b8d8443
Tim <tim.guenther@live.de>
2015-08-12 18:39:32 +0200

    check for decoding of  (deflated/url encoded/base64 content)

---
+++ src/main/java/de/rub/nds/burp/utilities/Encoding.java
@@ -0,0 +1,108 @@
+/**
+ * EsPReSSO - Extension for Processing and Recognition of Single Sign-On Protocols.
+ * Copyright (C) 2015/ Tim Guenther and Christian Mainka
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package de.rub.nds.burp.utilities;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.DataFormatException;
+
+/**
+ * Analyse data for different encodings.
+ * @author Tim Guenther
+ * @version 1.0
+ */
+public abstract class Encoding {
+    public static final int URL_ENCODED = 1;
+    public static final int BASE64_ENCODED = 2;
+    public static final int DEFLATED = 3;
+    
+    /**
+     * Search for the encoding type of the data.
+     * @param data The encoded data.
+     * @return The integer flags for the encoding, -1 if no encoding is detected.
+     */
+    public static int getEncoding(String data){
+        
+        if(isURLEncoded(data)){
+            return URL_ENCODED;
+        } else if(isBase64Encoded(data)){
+            return BASE64_ENCODED;
+        } else try {
+            if(isDeflated(data.getBytes("ASCII"))){
+                return DEFLATED;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Encoding.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
+    }
+    
+    /**
+     * Check for URL encoding.
+     * @param data The encoded data.
+     * @return True if the encoding is URL Encoding, otherwise false.
+     */
+    public static boolean isURLEncoded(String data){
+        boolean flag = true;
+        try {
+            URLDecoder.decode(data, "ASCII");
+        } catch (UnsupportedEncodingException ex) {
+            flag = false;
+        }
+        String pattern = "%[a-zA-Z0-9]{2}";
+        return (flag && regex_contains(pattern, data));
+    }
+    
+    /**
+     * Check for Base64 encoding.<br>
+     * Source for pattern see: {@code http://stackoverflow.com/questions/8571501/how-to-check-whether-the-string-is-base64-encoded-or-not}
+     * @param data The encoded data.
+     * @return True if the encoding is Base 64 Encoding, otherwise false.
+     */
+    public static boolean isBase64Encoded(String data){
+        String pattern = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$";
+        return regex_contains(pattern, data);
+    }
+    
+    /**
+     * Check for deflated.
+     * @param data The compressed data.
+     * @return True if the encoding is URL Encoding, otherwise false.
+     * @throws java.io.IOException If the Compression class fails.
+     */
+    public static boolean isDeflated(byte[] data) throws IOException{
+        try{
+            Compression.decompress(data);
+        } catch(DataFormatException e){
+            return false;   
+        }
+        return true; 
+    }
+    
+    private static boolean regex_contains(String pattern, String data){
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(data);
+        return m.find();
+    }
+}
---
+++ src/test/java/de/rub/nds/burp/utilities/EncodingTest.java
@@ -0,0 +1,93 @@
+/**
+ * EsPReSSO - Extension for Processing and Recognition of Single Sign-On Protocols.
+ * Copyright (C) 2015/ Tim Guenther and Christian Mainka
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package de.rub.nds.burp.utilities;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author ackbar
+ */
+public class EncodingTest extends TestCase {
+    
+    public EncodingTest(String testName) {
+        super(testName);
+    }
+
+
+    /**
+     * Test of isURLEncoded method, of class EncodingChecker.
+     */
+    public void testIsURLEncoded() {
+        System.out.println("isURLEncoded");
+        String data = "This%20is%20a%20test%20string%20with%20special%20chars%20like%20ths%3A%20%2f@%5E%26%2a%28%28%29%7B%7D%7B%3A%3B";
+        boolean result = Encoding.isURLEncoded(data);
+        assertTrue(result);
+        
+        data = " /!@#$*()[]{}";
+        result = Encoding.isURLEncoded(data);
+        assertFalse(result);
+    }
+
+    /**
+     * Test of isBase64Encoded method, of class EncodingChecker.
+     */
+    public void testIsBase64Encoded() {
+        System.out.println("isBase64Encoded");
+        
+        String data = "VGhpcyBpcyBhIHRlc3Qgc3RyaW5nIHdpdGggc3BlY2lhbCBjaGFycyBsaWtlIHRoczogL0BeJiooKCl7fXs6Ow==";
+        boolean result = Encoding.isBase64Encoded(data);
+        assertTrue(result);
+        
+        String data1 = "This is a test string with special chars like ths: /@^&*((){}{:;";
+        boolean result1 = Encoding.isBase64Encoded(data1);
+        assertFalse(result1);
+        
+    }
+
+    /**
+     * Test of isDeflated method, of class EncodingChecker.
+     */
+    public void testIsDeflated() {
+        System.out.println("isDeflated");
+        String data = "fZFba8MwDIX%2FStB7c11a19QpYaNQ2GCsl4e9DDdR2rDEziy79OfP9MI6BnsUOp%2BOdDSbn%2FouOKKhVisBSRhDgKrSdav2AjbrxYjBvJiR7Lt04KWzB%2FWGXw7JBh5UxC8dAc4oriW1xJXskbit%2BKp8eeZpGPPBaKsr3UFQEqGx3upRK3I9mhWaY1vhUtV4EuDNS2tNu3MWLwq%2Fxh%2FJQpsKz6sIaGRHCMHyScAHm%2BwaTOo8G0uZxslDlu8mdTOOMzaVjE0bL6NXSdQe8Qckcn40WamsAE%2Flo5iNknSdpDxPeRaHLJu%2BQ7C9JeTvgWse%2FAyb%2ByD%2Bz0HerofiYO3Ao8ijW206JKf2IQ1hpftIadPLbq0%2FUc2ie6PiWv7%2BQ%2FEN";
+        try {
+            data = URLDecoder.decode(data, "ASCII");
+        } catch (UnsupportedEncodingException ex) {
+            fail(ex.toString());
+        }
+        Decoder d = Base64.getDecoder();
+        byte[] bin_data = d.decode(data);
+        
+        boolean result = false;
+        try {
+            result = Encoding.isDeflated(bin_data);
+        } catch (IOException ex) {
+            fail(ex.toString());
+        }
+        
+        assertTrue(result);
+    }
+    
+}
