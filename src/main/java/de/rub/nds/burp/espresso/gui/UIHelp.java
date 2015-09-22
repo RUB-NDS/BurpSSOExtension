c7a21196b9d5af081907015300123f0f714edc3b
Tim <tim.guenther@live.de>
2015-09-22 13:13:33 +0200

    Update with about

--- src/main/java/de/rub/nds/burp/espresso/gui/UIHelp.java
+++ src/main/java/de/rub/nds/burp/espresso/gui/UIHelp.java
@@ -30,6 +30,10 @@ public class UIHelp extends javax.swing.JPanel {
      */
     public UIHelp() {
         initComponents();
+        
+        //Remove if help is implemented.
+        helpContainerPanel.setEnabledAt(0, false);
+        helpContainerPanel.setEnabledAt(1, false);
     }
 
     /**
@@ -42,7 +46,6 @@ public class UIHelp extends javax.swing.JPanel {
     private void initComponents() {
 
         headline = new javax.swing.JLabel();
-        subheadline = new javax.swing.JLabel();
         helpContainerPanel = new javax.swing.JTabbedPane();
         apiPanel = new javax.swing.JPanel();
         protocolsTabbedPane = new javax.swing.JTabbedPane();
@@ -58,28 +61,42 @@ public class UIHelp extends javax.swing.JPanel {
         oauthPane = new javax.swing.JTabbedPane();
         oauth1Pane = new javax.swing.JPanel();
         oauth2Pane = new javax.swing.JPanel();
+        impressum = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
 
         setEnabled(false);
 
-        headline.setText("Help for Using this Extension at: https://github.com/");
+        headline.setText("Help for Using this Extension at: https://github.com/RUB-NDS/EsPReSSO");
 
-        subheadline.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
-        subheadline.setText("Help in progress!");
-
-        helpContainerPanel.setEnabled(false);
+        apiPanel.setEnabled(false);
 
         javax.swing.GroupLayout apiPanelLayout = new javax.swing.GroupLayout(apiPanel);
         apiPanel.setLayout(apiPanelLayout);
         apiPanelLayout.setHorizontalGroup(
             apiPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 518, Short.MAX_VALUE)
+            .addGap(0, 599, Short.MAX_VALUE)
         );
         apiPanelLayout.setVerticalGroup(
             apiPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 636, Short.MAX_VALUE)
+            .addGap(0, 662, Short.MAX_VALUE)
         );
 
         helpContainerPanel.addTab("API", apiPanel);
+
+        protocolsTabbedPane.setEnabled(false);
+
+        samlPane.setEnabled(false);
 
         smal1Pane.setEnabled(false);
         smal1Pane.setFocusable(false);
@@ -88,11 +105,11 @@ public class UIHelp extends javax.swing.JPanel {
         smal1Pane.setLayout(smal1PaneLayout);
         smal1PaneLayout.setHorizontalGroup(
             smal1PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 508, Short.MAX_VALUE)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
         smal1PaneLayout.setVerticalGroup(
             smal1PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 582, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
 
         samlPane.addTab("SAML 1.0", smal1Pane);
@@ -101,11 +118,11 @@ public class UIHelp extends javax.swing.JPanel {
         saml2Pane.setLayout(saml2PaneLayout);
         saml2PaneLayout.setHorizontalGroup(
             saml2PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 508, Short.MAX_VALUE)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
         saml2PaneLayout.setVerticalGroup(
             saml2PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 582, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
 
         samlPane.addTab("SAML 2.0", saml2Pane);
@@ -119,11 +136,11 @@ public class UIHelp extends javax.swing.JPanel {
         flowchartPane.setLayout(flowchartPaneLayout);
         flowchartPaneLayout.setHorizontalGroup(
             flowchartPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 508, Short.MAX_VALUE)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
         flowchartPaneLayout.setVerticalGroup(
             flowchartPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 582, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
 
         browseridPane.addTab("Flowchart", flowchartPane);
@@ -134,11 +151,11 @@ public class UIHelp extends javax.swing.JPanel {
         openid11Pane.setLayout(openid11PaneLayout);
         openid11PaneLayout.setHorizontalGroup(
             openid11PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 508, Short.MAX_VALUE)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
         openid11PaneLayout.setVerticalGroup(
             openid11PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 582, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
 
         openidPane.addTab("OpenID 1.1", openid11Pane);
@@ -147,11 +164,11 @@ public class UIHelp extends javax.swing.JPanel {
         openid2Pane.setLayout(openid2PaneLayout);
         openid2PaneLayout.setHorizontalGroup(
             openid2PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 508, Short.MAX_VALUE)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
         openid2PaneLayout.setVerticalGroup(
             openid2PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 582, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
 
         openidPane.addTab("OpenID 2.0", openid2Pane);
@@ -162,11 +179,11 @@ public class UIHelp extends javax.swing.JPanel {
         openidconnectPane.setLayout(openidconnectPaneLayout);
         openidconnectPaneLayout.setHorizontalGroup(
             openidconnectPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 513, Short.MAX_VALUE)
+            .addGap(0, 594, Short.MAX_VALUE)
         );
         openidconnectPaneLayout.setVerticalGroup(
             openidconnectPaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 609, Short.MAX_VALUE)
+            .addGap(0, 635, Short.MAX_VALUE)
         );
 
         protocolsTabbedPane.addTab("OpenID Connect", openidconnectPane);
@@ -175,11 +192,11 @@ public class UIHelp extends javax.swing.JPanel {
         oauth1Pane.setLayout(oauth1PaneLayout);
         oauth1PaneLayout.setHorizontalGroup(
             oauth1PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 508, Short.MAX_VALUE)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
         oauth1PaneLayout.setVerticalGroup(
             oauth1PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 582, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
 
         oauthPane.addTab("OAuth 1.0", oauth1Pane);
@@ -188,11 +205,11 @@ public class UIHelp extends javax.swing.JPanel {
         oauth2Pane.setLayout(oauth2PaneLayout);
         oauth2PaneLayout.setHorizontalGroup(
             oauth2PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 508, Short.MAX_VALUE)
+            .addGap(0, 589, Short.MAX_VALUE)
         );
         oauth2PaneLayout.setVerticalGroup(
             oauth2PaneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 582, Short.MAX_VALUE)
+            .addGap(0, 608, Short.MAX_VALUE)
         );
 
         oauthPane.addTab("OAuth 2.0", oauth2Pane);
@@ -201,20 +218,100 @@ public class UIHelp extends javax.swing.JPanel {
 
         helpContainerPanel.addTab("Protocols", protocolsTabbedPane);
 
+        jLabel1.setText("© 2015, Tim Guenther, Christian Mainka");
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel2.setText("EsPReSSO");
+
+        jLabel3.setText("Extension for Processing and Recognition of Single Sign-On");
+
+        jLabel4.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLabel4.setText("Dependencies");
+
+        jLabel5.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLabel5.setText("License");
+
+        jLabel6.setText("GNU General Public License v2.0");
+
+        jLabel7.setText("RSyntaxTextArea, modified BSD license, © 2012, Robert Futrell");
+
+        jLabel8.setText("https://github.com/bobbylight/RSyntaxTextArea");
+
+        jLabel9.setText("json-simple, Apache License 2.0, © Yidong Fang");
+
+        jLabel10.setText("https://code.google.com/p/json-simple/ ");
+
+        jLabel11.setText("WS-Attacker, GNU General Public License v2.0, © 2012, Mainka, Falkenberg, et al.");
+
+        jLabel12.setText("https://github.com/RUB-NDS/WS-Attacker/");
+
+        javax.swing.GroupLayout impressumLayout = new javax.swing.GroupLayout(impressum);
+        impressum.setLayout(impressumLayout);
+        impressumLayout.setHorizontalGroup(
+            impressumLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(impressumLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(impressumLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel8)
+                    .addComponent(jLabel9)
+                    .addComponent(jLabel10)
+                    .addComponent(jLabel11)
+                    .addComponent(jLabel12))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        impressumLayout.setVerticalGroup(
+            impressumLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(impressumLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel6)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel8)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel9)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel10)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel11)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel12)
+                .addContainerGap(345, Short.MAX_VALUE))
+        );
+
+        helpContainerPanel.addTab("About", impressum);
+
+        helpContainerPanel.setSelectedIndex(2);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(5, 5, 5)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(headline)
-                            .addComponent(subheadline))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addComponent(helpContainerPanel))
-                .addGap(6, 6, 6))
+                .addComponent(headline)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(helpContainerPanel)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -222,10 +319,8 @@ public class UIHelp extends javax.swing.JPanel {
                 .addGap(4, 4, 4)
                 .addComponent(headline)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(subheadline)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(helpContainerPanel)
-                .addGap(17, 17, 17))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -236,6 +331,19 @@ public class UIHelp extends javax.swing.JPanel {
     private javax.swing.JPanel flowchartPane;
     private javax.swing.JLabel headline;
     private javax.swing.JTabbedPane helpContainerPanel;
+    private javax.swing.JPanel impressum;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel oauth1Pane;
     private javax.swing.JPanel oauth2Pane;
     private javax.swing.JTabbedPane oauthPane;
@@ -247,6 +355,5 @@ public class UIHelp extends javax.swing.JPanel {
     private javax.swing.JPanel saml2Pane;
     private javax.swing.JTabbedPane samlPane;
     private javax.swing.JPanel smal1Pane;
-    private javax.swing.JLabel subheadline;
     // End of variables declaration//GEN-END:variables
 }
